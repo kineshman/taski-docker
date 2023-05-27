3cfbe91bf0df05188cce6544ad4c56596056051b
vlad Tokarev <vladiktokarev97@gmail.com>
2023-05-27 10:42:35 +0300

    10 deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,12 +1,8 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
@@ -14,8 +10,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
